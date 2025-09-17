ecc56a7107d72a8e61a52dfe80a7c1648a839da7
VaniaVI <vania.vargas.igor@gmail.com>
2025-09-17 18:23:37 -0300

    Se arregla problema de detalleProducto del home

--- js/index.js
+++ js/index.js
@@ -1,33 +1,28 @@
-
-  // 👉 Aquí defines tus productos
-// 👉 Aquí defines tus productos Tendencias
-    const productos = [
-      { nombre: "Producto 1", precio: "9.990", descripcion: "Descripción breve del producto 1", imagen: "https://i.pinimg.com/736x/6a/89/48/6a8948b559735c80860cdf2169efc874.jpg" },
-      { nombre: "Producto 2", precio: "12.990", descripcion: "Descripción breve del producto 2", imagen: "https://i.pinimg.com/736x/66/37/c3/6637c3eb58091edca0f717094b341e67.jpg" },
-      { nombre: "Producto 3", precio: "7.490", descripcion: "Descripción breve del producto 3", imagen: "https://i.pinimg.com/736x/38/9f/6e/389f6e9fb212bd7d46771ead96f90603.jpg" },
-      { nombre: "Producto 4", precio: "15.990", descripcion: "Descripción breve del producto 4", imagen: "https://i.pinimg.com/736x/00/07/58/00075847c7272a8fbab818c8d4b3d372.jpg" },
-      { nombre: "Producto 4", precio: "15.990", descripcion: "Descripción breve del producto 4", imagen: "https://i.pinimg.com/736x/00/07/58/00075847c7272a8fbab818c8d4b3d372.jpg" }
+import { productos } from "./productos.js";
+// 👉 Contenedor donde van las cards
+const container = document.getElementById("productTrendContainer");
     
-    ];
+const productosTendencias = productos.filter(p => p.filtro === "tendencias")
 
-    // 👉 Contenedor donde van las cards
-    const container = document.getElementById("productTrendContainer");
-
-    // 👉 Crear dinámicamente las cards
-    productos.forEach(p => {
-      const card = `
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="card custom-card h-100">
-            <img src="${p.imagen}" class="card-img-top h-80" alt="${p.nombre}">
-            <div class="card-body d-flex flex-column">
-              <h5 class="card-title">${p.nombre}</h5>
-              <div class="mt-auto d-flex justify-content-between align-items-center">
-                <span class="price">$${p.precio}</span>
-                <a href="detalleProducto.html" class="btn btn-sm btn-primary">Ver detalle</a>
-              </div>
+// 👉 Crear dinámicamente las cards
+if (productosTendencias.length === 0) {
+  container.innerHTML="<p>No hay productos en tendencia en estos momentos</p>";
+}else{
+  productosTendencias.forEach(p => {
+    const card = `
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="card custom-card h-100">
+          <img src="${p.imagen}" class="card-img-top h-80" alt="${p.nombre}">
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">${p.nombre}</h5>
+            <div class="mt-auto d-flex justify-content-between align-items-center">
+              <span class="price">$${p.precio}</span>
+               <a href="detalleProducto.html?prodid=${p.id}&cat=${p.categoria}" class="btn btn-sm btn-primary">Ver detalle</a>
             </div>
           </div>
         </div>
-      `;
-      container.innerHTML += card;
-    });
+      </div>
+    `;
+    container.innerHTML += card;
+  });
+}
--- js/productos.js
+++ js/productos.js
@@ -4,6 +4,7 @@ export const productos = [
     id: 1,
     nombre: "Collar de cuero",
     categoria: "accesorios",
+    filtro: "none",
     precio: "9.990",
     descripcion: "Para perros grandes",
     imagen: "https://ae-pic-a1.aliexpress-media.com/kf/Sc812fa59a89542cba4fdfb73c394ea06R.jpg_960x960q75.jpg_.avif"
@@ -12,6 +13,7 @@ export const productos = [
     id: 2,
     nombre: "Comida premium",
     categoria: "alimentos",
+    filtro: "none",
     precio: "12.990",
     descripcion: "Nutrición completa",
     imagen: "https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwe8e129cd/images/44_m.jpg"
@@ -20,6 +22,7 @@ export const productos = [
     id: 3,
     nombre: "Contenedor de comida y agua",
     categoria: "accesorios",
+    filtro: "none",
     precio: "7.490",
     descripcion: "Estimula el juego",
     imagen: "https://ae-pic-a1.aliexpress-media.com/kf/S6abf1509215e4a4fa3fcf63d70d6d091T.jpg_960x960q75.jpg_.avif"
@@ -28,6 +31,7 @@ export const productos = [
     id: 4,
     nombre: "Snack saludable",
     categoria: "alimentos",
+    filtro: "none",
     precio: "15.990",
     descripcion: "Sin colorantes",
     imagen: "https://www.superzoo.cl/on/demandware.static/-/Sites-SuperZoo-master-catalog/default/dwdd89c052/images/sku14881_1.jpg"
@@ -36,9 +40,56 @@ export const productos = [
     id: 5,
     nombre: "Pack de ofertas",
     categoria: "ofertas especiales",
+    filtro: "none",
     precio: "19.990",
     descripcion: "Combo especial",
     imagen: "https://i.pinimg.com/736x/6a/89/48/6a8948b559735c80860cdf2169efc874.jpg"
+  },
+  {
+    id: 6,
+    nombre: "Kit Pet Glam",
+    categoria: "accesorios",
+    filtro: "tendencias",
+    precio: "10.990",
+    descripcion: "Set de accesorios con estilo para mascotas modernas",
+    imagen: "https://ae-pic-a1.aliexpress-media.com/kf/S1285775e15bf4813ab42bf7525bd6cee9.jpg_960x960q75.jpg_.avif"
+  },
+  {
+    id: 7,
+    nombre: "Snack Box Premium",
+    categoria: "alimentos",
+    filtro: "tendencias",
+    precio: "18.990",
+    descripcion: "Caja sorpresa con snacks gourmet para tu peludo",
+    imagen: "https://www.moxypetbox.cl/cdn/shop/files/Caja_Suscripci_n_Para_Perros.jpg?crop=center&height=1200&v=1750890539&width=1200"
+  },
+  {
+    id: 8,
+    nombre: "Combo Relax Pet",
+    categoria: "accesorios",
+    filtro: "tendencias",
+    precio: "22.990",
+    descripcion: "Almohadilla + aromaterapia para mascotas ansiosas",
+    imagen: "https://ae01.alicdn.com/kf/S75091b79a2284529a5421ade36a4f4888.jpg"
+  },
+
+  {
+    id: 9,
+    nombre: "Outfit Urban Pet",
+    categoria: "ropa",
+    filtro: "tendencias",
+    precio: "29.990",
+    descripcion: "Ropa urbana para mascotas con actitud",
+    imagen: "https://ae01.alicdn.com/kf/Sd16d48cfa4e845a2905eba188d4a5b70F.jpg"
+  },
+  {
+    id: 10,
+    nombre: "Pack EcoPet",
+    categoria: "accesorios",
+    filtro: "none",
+    precio: "21.990",
+    descripcion: "Productos sustentables para cuidar a tu mascota y al planeta",
+    imagen: "https://i.pinimg.com/736x/7d/fe/56/7dfe56a6b62c147368f5d8ae7780d56b.jpg"
   }
 ];
 
